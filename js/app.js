800bcecc88cc5acfa2350d1611bd153207d46c3d
RebootJeff <RebootJeff@users.noreply.github.com>
2015-07-27 15:06:02 -0700

    Add more form inputs

--- js/app.js
+++ js/app.js
@@ -1,10 +1,34 @@
-angular.module('app', ['ngMaterial', 'firebase']);
+angular.module('app', [
+  'ngMaterial',
+  'firebase'
+]);
 
 angular.module('app').controller('MainCtrl', function($firebaseObject) {
   var ctrl = this;
 
-  var FIREBASE_URL = 'https://flickering-torch-1679.firebaseio.com/';
+  var FIREBASE_URL = 'https://flickering-torch-1679.firebaseio.com/poll';
   var ref = new Firebase(FIREBASE_URL);
 
-  ctrl.question = 'hello';
+  var poll = $firebaseObject(ref);
+
+  poll.$loaded().then(function() {
+    ctrl.question = poll.question || '';
+    ctrl.choices = poll.choices || [{}];
+  });
+
+  ctrl.CHOICE_TYPES = [
+    'checkboxes',
+    'radio buttons'
+  ];
+
+  ctrl.addChoice = function() {
+    ctrl.choices.push({});
+  };
+
+  ctrl.submit = function() {
+    console.log('poll:', poll);
+    poll.question = ctrl.question;
+    poll.choices = ctrl.choices;
+    poll.$save();
+  };
 });
